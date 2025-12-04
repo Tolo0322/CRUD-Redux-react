0ad0a31209bafef59c9b1184319e1b05d22b7cdc
null <null>
2025-12-04 16:12:02 -0500

    creacion de reducer addTASK Y deleteTask

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,18 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import { useSelector } from 'react-redux';
 import TaskList from './components/taskList';
 import TaskForm from './components/taskForm';
 
 function App() {
-  const taskState = useSelector((state) => state.tasks);
-  console.log(taskState);
+
 
   return (
     <>
       <div>
         <h1>Hello</h1>
-        <TaskList />
         <TaskForm />
+        <TaskList />
       </div>
     </>
   );
--- src/components/taskList.jsx
+++ src/components/taskList.jsx
@@ -1,5 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
+import { deleteTask } from "../features/tasks/taskSlice";
+
 const TaskList = () => {
-  return <div>TaskList</div>;
+
+  const tasks = useSelector(state => state.tasks)
+  const dispatch = useDispatch()
+
+  const handleDelete = (id) => {
+    dispatch(deleteTask(id))
+  }
+
+  return (
+    <div>
+      {tasks.map(task => (
+        <div key={task.id}>
+          <h3>{task.title}</h3>
+          <p>{task.description}</p>
+          <button onClick={() => handleDelete(task.id)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  )
 };
 
 export default TaskList;
